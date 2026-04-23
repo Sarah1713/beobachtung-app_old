05b8262a478e1718290649c8ab1d4a704c37ef99
Sarah1713 <sarah.julia.weisshart@uni-jena.de>
2026-04-23 13:52:12 +0200

    sw.js

--- sw.js
+++ sw.js
@@ -1,23 +1,20 @@
-const CACHE_NAME = "moment-cache-v1";
+const CACHE_NAME = "moment-app-v1";
 const urlsToCache = [
-  "index.html",
-  "manifest.json"
+  "./",
+  "./index.html",
+  "./manifest.json"
 ];
 
-self.addEventListener("install", function(event) {
+self.addEventListener("install", event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then(function(cache) {
-        return cache.addAll(urlsToCache);
-      })
+      .then(cache => cache.addAll(urlsToCache))
   );
 });
 
-self.addEventListener("fetch", function(event) {
+self.addEventListener("fetch", event => {
   event.respondWith(
     caches.match(event.request)
-      .then(function(response) {
-        return response || fetch(event.request);
-      })
+      .then(response => response || fetch(event.request))
   );
 });
